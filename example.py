e5304c14f7b4024afa317593db2764755e2f2023
NotPeOpLe <zombieasura@outlook.com>
2020-12-16 23:38:18 +0800

    新增註冊玩家清單範本資料

--- example.py
+++ example.py
@@ -1,1 +1,34 @@
+
 staff = {'Host': [{'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Host'}], 'Referee': [{'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Referee'}, {'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Referee'}, {'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Referee'}], 'Streamer': [{'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Streamer'}, {'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Streamer'}], 'Commentator': [{'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Commentator'}, {'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Commentator'}, {'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Commentator'}], 'Designer': [{'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Designer'}], 'Map Selector': [{'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Map Selector'}, {'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Map Selector'}, {'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Map Selector'}], 'Supporter': [{'username': '_CHIMERA', 'user_id': 6008293, 'group': 'Supporter'}], 'PAPA': [{'username': '_CHIMERA', 'user_id': 6008293, 'group': 'PAPA'}]}
+registered_list = [
+    {
+        'username': '_CHIMERA',
+        'user_id': 6008293,
+        'message': 'Hi!'
+    },
+    {
+        'username': '_CHIMERA',
+        'user_id': 6008293,
+        'message': 'Hi!'
+    },
+    {
+        'username': '_CHIMERA',
+        'user_id': 6008293,
+        'message': 'Hi!'
+    },
+    {
+        'username': '_CHIMERA',
+        'user_id': 6008293,
+        'message': 'Hi!'
+    },
+    {
+        'username': '_CHIMERA',
+        'user_id': 6008293,
+        'message': 'Hi!'
+    },
+    {
+        'username': '_CHIMERA',
+        'user_id': 6008293,
+        'message': 'Hi!'
+    },
+]
