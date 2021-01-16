896fd8f5f5cdf301a6891d7b4d444fba6e4b843d
NotPeOpLe <zombieasura@outlook.com>
2021-01-17 07:12:45 +0800

    新增方法

--- mysql.py
+++ mysql.py
@@ -65,3 +65,67 @@ class DB(object):
         self.cursor.close()
         # 断开数据库连接
         self.connect.close()
+
+    def get_mappool(self, round_id, ingore_pool_publish = False):
+        """
+        取得圖池: round_id = 階段ID
+        """
+        # 初始化 mappool
+        mappool = {}
+        # 從資料庫取得階段資訊
+        round = self.query_one(f"SELECT * FROM round WHERE id = {round_id}")
+        # 檢查此階段的圖池是否已公布
+        if round['pool_publish'] and not ingore_pool_publish: 
+            # 從資料庫取得圖池
+            pooldata = self.query_all(f"SELECT `id`, `beatmap_id`, `group`, `code`, `mods`, `info` FROM mappool WHERE round_id = {round_id} ORDER BY FIELD(`group`, 'NM', 'HD', 'HR', 'DT', 'FM', 'Roll', 'EZ', 'TB'), code")
+        
+            # map['info'] 轉化為 dict 類型
+            for map in pooldata:
+                map['info'] = json.loads(map['info'])
+
+            # 將 map 以 group 分類
+            for map in pooldata:
+                # 如果這 map 的 group 沒有在 mappool.keys() 裡面，則創建它
+                if map['group'] not in mappool.keys():
+                    mappool[map['group']] = []
+
+                # 將 map 添加到 mappool 的 group 裡
+                mappool[map['group']].append(map)
+            
+        return {
+            'round_id': int(round_id),
+            'mappool': mappool
+        }
+
+    @property
+    def active_rounds(self):
+        """
+        取得目前已開始的階段
+        """
+        rounds = self.query_all("SELECT * FROM round WHERE start_date < NOW()")
+        return rounds
+
+    def get_teams(self, id=None):
+        query_team_text = "SELECT * FROM team"
+        if id:
+            query_team_text += " WHERE id = %d" % id
+
+        teams = self.query_all(query_team_text)
+        for t in teams:
+            t['players'] = []
+            players = self.query_all("SELECT * FROM player WHERE team_id = %s", (t['id'],))
+            for p in players:
+                p['info'] = json.loads(p['info'])
+                p['bp1'] = json.loads(p['bp1'])
+                t['players'].append(p)
+        return teams
+
+    def get_players(self, id=None):
+        query_text = "SELECT * FROM player"
+        if id:
+            query_text += " WHERE id = %d" % id
+        players = self.query_all(query_text)
+        for p in players:
+                p['info'] = json.loads(p['info'])
+                p['bp1'] = json.loads(p['bp1'])
+        return players
